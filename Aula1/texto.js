e470a086241cd349e7354ef612ff8b167c1ce30e
tarcisio <tarcisioemanoel10031@gmail.com>
2022-03-17 23:01:50 -0300

    EXCpfsEApiss

--- Aula1/texto.js
+++ Aula1/texto.js
@@ -20,9 +20,25 @@ Joooooooãooooooo
 const html = '<p>Ola Mundo</p> <p>Hello world</p> <div> Sou a div </div>';
 
 const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ atenção abcddefghhijklmnopqrstuvwxyz @! 0123456789';
+
+const cpfs = `
+Os cpfs são:
+254.224.887-45 215.978.456-12 047.258.396-96 
+936.987.321-00
+`;
+
+const ips = `
+Os IPS são:
+
+0.0.0.0 192.168.0.25
+
+10.10.5.12 255.255.255.255
+`;
+
 module.exports = {
   tx,
   arquivo,
   html,
-  alfabeto
+  alfabeto,
+  cpfs, ips
 } 
