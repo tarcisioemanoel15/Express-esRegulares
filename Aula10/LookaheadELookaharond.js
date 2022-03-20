3ebb6390ba6b07d6bf12232d9544aaaa6bfd4bb9
tarcisio <tarcisioemanoel10031@gmail.com>
2022-03-20 20:49:48 -0300

    nomits

--- Aula10/LookaheadELookaharond.js
+++ Aula10/LookaheadELookaharond.js
@@ -1,5 +1,21 @@
 const { loock } = require('../Aula1/texto');
 
-console.log(loock)
+console.log(loock);
 
-  /
+// termina com active
+// console.log(loock.match(/.+[^in]active$/gim));
+
+// Positivo LOOKAHEAD (Frases que tem active)
+// console.log(loock.match(/.+(?=[^in]active)/gim));
+
+// Positivo LOOKAHEAD (Frases que tem inactive)
+// console.log(loock.match(/.+(?=\s+inactive)/gim));
+
+// Negativo LOOKAHEAD (Frases que não tem active)
+// console.log(loock.match(/^(?!.+[^in]active).+$/gim));
+
+// Positive lookbehind (frases que comessam com online)
+// console.log(loock.match(/(?<=online\s+)\S+.*/gim));
+
+// Negative lookbehind (frases que não comessam com online)
+// console.log(loock.match(/^.+(?<!online.+)$/gim));
