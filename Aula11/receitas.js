edb0ae5d2548730dce7b55bc78f4a9f0b7909ca3
tarcisio <tarcisioemanoel10031@gmail.com>
2022-03-24 16:07:44 -0300

    Senhas fortes

---
+++ Aula11/receitas.js
@@ -0,0 +1,18 @@
+//para encontrar todas as palavras ate as asntuadas /[\wÁ-ú]+/gm
+
+//não numeros naoNumerosRegEx = /\D/gm
+
+//Valida IP ipRegExp  ipRegExp = /((25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]\d|\d)(\.)){3}(25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]\d|\d)/g;
+
+//valida cpf cpfregExp = /(?:\d{3}\.){2}\d{3}\-\d{2}/g;
+
+// telefone /^(\(\d{2}\)\s*)?(9\s*)?(\d{4})-(\d{4})/gm
+
+//senhas fortes /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#%]).{8}/gm
+
+
+/*
+E-mail
+Valida Email
+// /permisiva [^\s]+@[^\s]+(\.\w+) a*ll/
+*/
